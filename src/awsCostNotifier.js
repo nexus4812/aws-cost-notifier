ca1171053ffedc39f02a1c2cf77adb20d42d447b
nexus4812 <powerbatteryboy@gmail.com>
2024-03-17 18:20:10 +0900

    Create awsCostNotifier

--- src/awsCostNotifier.js
+++ src/awsCostNotifier.js
@@ -1,12 +1,32 @@
 import axios  from "axios";
+import moment from "moment/moment";
+import AWS from "aws-sdk"
 
 export async function handler(event, context) {
-    const response = await axios.post(process.env.DISCORD_DEVELOP_WEBHOOK_URL, {
-        content: "hello"
+    const now = moment();
+    const start = now.format('YYYY-MM-01');
+    const end = now.add(1, 'month').format('YYYY-MM-01');
+    const explorer = new AWS.CostExplorer({region: 'ap-northeast-1'});
+    const awsResponse = await explorer.getCostAndUsage({
+        TimePeriod: {
+            Start: start,
+            End: end
+        },
+        Granularity: 'MONTHLY',
+        Metrics: ['UNBLENDED_COST']
+    }).promise()
+
+    const detail = JSON.stringify(awsResponse.ResultsByTime);
+
+    const discordResponse = await axios.post(process.env.DISCORD_DEVELOP_WEBHOOK_URL, {
+        content: '今月のAWSコスト: ``` ' + detail + ' ```'
     });
 
     return {
-        response: response.data,
-        status: response.status
+        start: start,
+        end: end,
+        awsResponse: awsResponse.ResultsByTime,
+        discordResponse: discordResponse.data,
+        status: discordResponse.status
     }
 }
