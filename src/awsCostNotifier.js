1871496768485d13629bd8830be516afcff244a9
nexus4812 <powerbatteryboy@gmail.com>
2024-03-17 15:33:45 +0900

    Add ability to post messages

--- src/awsCostNotifier.js
+++ src/awsCostNotifier.js
@@ -1,5 +1,12 @@
+import axios  from "axios";
+
 export async function handler(event, context) {
+    const response = await axios.post(process.env.DISCORD_DEVELOP_WEBHOOK_URL, {
+        content: "hello"
+    });
+
     return {
-        message: "hello"
+        response: response.data,
+        status: response.status
     }
 }
